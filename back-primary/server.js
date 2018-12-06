4dd514d9530ecad061775c9a0a186cdf92de2c4d
Benoît Hubert <benoithubert@gmail.com>
2018-12-06 13:17:30 +0100

    Use cors

--- back-primary/server.js
+++ back-primary/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const query = require('./query');
 
 const app = express();
+app.use(cors());
 
 app.get('/ohlcv', (req, res) => query('select * from history_ohlcv')
   .then(records => res.json(records))
